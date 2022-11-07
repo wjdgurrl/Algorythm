5be102eb2d58e4f74a087244b0a58c913f68e856
wjdgurrl <gurrl8356@gmail.com>
2022-11-08 00:56:56 +0900

    2022-11-08

---
+++ src/programmers/course_120802.java
@@ -0,0 +1,18 @@
+package programmers;
+
+import static programmers.course_120802.Solution.solution;
+
+/*https://school.programmers.co.kr/learn/courses/30/lessons/120802?language=java*/
+public class course_120802 {
+    public static void main(String[] args) {
+        System.out.println(solution(2,3));
+
+
+    }
+    class Solution{
+        public static int solution(int num1, int num2){
+            int answer = num1+num2;
+            return answer;
+        }
+    }
+}
