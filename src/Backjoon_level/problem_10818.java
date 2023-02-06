fa1ebd4438797ea6332e64091b7c25fc74a517d5
wjdgurrl <gurrl8356@gmail.com>
2023-02-06 21:26:32 +0900

    2023-02-06

--- src/Backjoon_level/problem_10818.java
+++ src/Backjoon_level/problem_10818.java
@@ -4,8 +4,18 @@ import java.io.*;
 import java.util.*;
 
 public class problem_10818 {
-    public static void main(String[] args) {
+    public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        //ArrayList<Integer> array = new ArrayList<>();
+        String input = br.readLine();
+        st = new StringTokenizer(input);
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[0] + " " + arr[N - 1]);
     }
 }
