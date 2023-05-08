467d6275f0970cee22acf80afca4dcce08f52386
gurrl <gurrl8356@gmail.com>
2023-05-09 02:58:18 +0900

    23/05/09

---
+++ src/programmers/course_92334.java
@@ -0,0 +1,19 @@
+package programmers;
+import java.util.*;
+import java.io.*;
+public class course_92334 {
+    public static void main(String[] args) {
+
+    }
+    class aaa{
+        public int[] aa(String[] id_list, String[] report, int k){
+            int[] answer = {};
+            ArrayList<String> arr = new ArrayList<>();
+
+            return answer;
+        }
+    }
+    class id extends aaa{
+
+    }
+}
