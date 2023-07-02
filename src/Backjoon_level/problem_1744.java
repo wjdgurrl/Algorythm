71f26c70c511f10c3a138345e1ca2bad61557b21
gurrl <gurrl8356@gmail.com>
2023-07-03 02:28:45 +0900

    23/07/03

---
+++ src/Backjoon_level/problem_1744.java
@@ -0,0 +1,19 @@
+package Backjoon_level;
+
+import java.util.*;
+import java.io.*;
+
+public class problem_1744 {
+    static ArrayList<Integer> Arr = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        ArrayList<Integer> positive = new ArrayList<>();
+        ArrayList<Integer> negative = new ArrayList<>();
+
+
+    }
+    static public void solution(){
+
+    }
+}
---
+++ src/programmers/course_150368.java
@@ -0,0 +1,23 @@
+package programmers;
+import java.util.*;
+import java.io.*;
+public class course_150368 {
+    public static void main(String[] args) {
+        int[][] users = new int[][]{{40,10000},{25,10000}};
+        int[] emotions = new int[]{7000,9000};
+        course_150368 cc = new course_150368();
+        System.out.println(Arrays.toString(cc.solution(users, emotions)));
+    }
+    public int[] solution(int[][] users, int[] emoticons){
+        int[] answer = {};
+        for(int i = 0; i< users.length; i++){
+
+            for (int j = 0; j < users.length; j++) {
+
+            }
+        }
+
+
+        return answer;
+    }
+}
