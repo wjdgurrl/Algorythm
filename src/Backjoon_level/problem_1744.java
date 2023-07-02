5e4e5b137ba67fbb889107ed5222a6935ca0c559
gurrl <gurrl8356@gmail.com>
2023-07-03 04:42:31 +0900

    23/07/03

--- src/Backjoon_level/problem_1744.java
+++ src/Backjoon_level/problem_1744.java
@@ -8,12 +8,49 @@ public class problem_1744 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine());
-        ArrayList<Integer> positive = new ArrayList<>();
-        ArrayList<Integer> negative = new ArrayList<>();
+        boolean zero = false;
+        PriorityQueue<Integer> positive = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> negative = new PriorityQueue<>();
+        for (int i = 0; i < num; i++) {
+            int input = Integer.parseInt(br.readLine());
+            if (input < 0) {
+                negative.add(input);
+            } else if (input > 0) {
+                positive.add(input);
+            } else {
+                zero = true;
+            }
+        }
 
-
+        System.out.println(solution(positive,negative,zero));
+        br.close();
     }
-    static public void solution(){
-
+    static public int solution(PriorityQueue<Integer> positive, PriorityQueue<Integer> negative, boolean zero){
+        int answer = 0;
+        while(!positive.isEmpty()){
+            Integer first = positive.poll();
+            Integer second = positive.poll();
+            if(second == null){
+                answer += first;
+                break;
+            }
+            if(second == 1){
+                answer += first + second;
+                continue;
+            }
+            answer += first * second;
+        }
+        while(!negative.isEmpty()){
+            Integer first = negative.poll();
+            Integer second = negative.poll();
+            if(second == null){
+                if(!zero) answer += first;
+                break;
+            }
+            else{
+                answer += first * second;
+            }
+        }
+        return answer;
     }
 }
