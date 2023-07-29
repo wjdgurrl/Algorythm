9c143ad68fe95fa1ad1bc4e1bbfbe561bbc29384
mac <gurrl8356@gmail.com>
2023-07-29 17:57:03 +0900

    23/07/29

--- src/programmers/course_150368.java
+++ src/programmers/course_150368.java
@@ -8,16 +8,31 @@ public class course_150368 {
         course_150368 cc = new course_150368();
         System.out.println(Arrays.toString(cc.solution(users, emotions)));
     }
+    static int[] sales = new int[]{10,20,30,40};
+    static ArrayList<int[]> arr = new ArrayList<>();
     public int[] solution(int[][] users, int[] emoticons){
         int[] answer = {};
-        for (int i = 0; i < users.length; i++) {
-
-        }
-
-
+        aaa(users,emoticons,0);
+        System.out.println(Arrays.toString(arr.get(0)));
         return answer;
     }
-    public int aaa(int n){
-        return n;
+    public void aaa(int[][] users, int[] emoticons,int depth){
+        if(depth == users.length) return;
+        int[] result = new int[users.length]; // {구매비용}
+
+        for (int i = 0; i < users.length; i++) {
+            for (int j = 0; j < sales.length; j++) {
+                if (users[i][0] <= sales[j]) {
+                    int purchase = emoticons[depth] * (100 - users[i][0]) / 100;
+                    result[i] = purchase;
+                    break;
+                }
+                if(users[i][0] > sales[j]){
+                    result[i] = emoticons[depth];
+                }
+            }
+        }
+        arr.add(result);
+        aaa(users, emoticons, ++depth);
     }
 }
