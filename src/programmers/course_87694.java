2c92a5a77d4f265e370a31f8f976bae331e80bb4
gurrl <gurrl8356@gmail.com>
2025-03-22 23:30:36 +0900

    25/03/22 87694 풀이예정

--- src/programmers/course_87694.java
+++ src/programmers/course_87694.java
@@ -7,5 +7,6 @@ import java.io.*;
 public class course_87694 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.close();
     }
 }
