677af349e13f6b6aa3c15b6d8dcdcddbc3287ec7
gurrl <gurrl8356@gmail.com>
2025-05-13 01:40:02 +0900

    25/05/13 13335 풀이예정

---
+++ src/Backjoon_level/problem_13335.java
@@ -0,0 +1,12 @@
+package Backjoon_level;
+
+import java.util.*;
+import java.io.*;
+
+//https://www.acmicpc.net/problem/13335
+public class problem_13335 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
