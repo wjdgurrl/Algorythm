002ecf328503f966ca487aece77451d24fce4090
gurrl <gurrl8356@gmail.com>
2025-06-10 10:30:17 +0900

    25/06/10
    1600
    풀이완료
    
    -
    풀이예정
    
    13460
    실패목록

---
+++ src/study/string/Note.java
@@ -0,0 +1,15 @@
+package study.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Note {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+    }
+
+
+}
