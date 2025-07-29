ed29cb20fe0b224d2112462929bdc7de6b5d9ffb
gurrl <gurrl8356@gmail.com>
2025-07-29 15:32:40 +0900

    25/07/29
    -
    풀이완료
    
    11057
    
    풀이예정
    
    16236
    13460
    5373
    실패목록

---
+++ src/Backjoon_level/problem_11057.java
@@ -0,0 +1,12 @@
+package Backjoon_level;
+
+import java.util.*;
+import java.io.*;
+
+//https://www.acmicpc.net/problem/11057
+public class problem_11057 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+    }
+}
