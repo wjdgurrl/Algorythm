1585678b290ca66b8e9d7dc01a0c8d373467c22a
gurrl <gurrl8356@gmail.com>
2025-07-30 17:47:32 +0900

    25/07/30
    11057
    풀이완료
    
    -
    풀이예정
    
    16236
    13460
    5373
    실패목록

--- src/Backjoon_level/problem_11057.java
+++ src/Backjoon_level/problem_11057.java
@@ -8,5 +8,27 @@ public class problem_11057 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        long count = 0;
+        long[][] dp = new long[N+1][10]; //자리수가 i일때 끝자리가 j
+        for(int j=0; j < 10; j++){
+            dp[1][j] = 1;
+        }
+
+        if(N > 1){
+            for(int i=2; i <= N; i++){
+                for(int j=0; j < 10; j++){
+                    for(int k=0; k<=j; k++){
+                        dp[i][j] += dp[i-1][k] % 10007;
+                    }
+
+                }
+            }
+
+        }
+        for(int i=0; i<10; i++){
+            count += dp[N][i] % 10007;
+        }
+        System.out.println(count % 10007);
+
     }
 }
